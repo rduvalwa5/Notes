a644be18723e1eca3f961f07e84ce09bf33fe261
rduvalwa5 <rduvalwa5@hotmail.com>
2017-08-15 21:12:44 -0700

    add more

---
+++ Programming Notes/SQL Doc/passwd_insert.sql
@@ -0,0 +1,39 @@
+-- comcast
+insert into uuid values('rduvalwa2','1',SYSDATE());
+insert into password values('blu4jazz','1',SYSDATE());
+
+-- hotmail
+insert into uuid values('rduvalwa5@hotmail.com','2',SYSDATE());
+insert into password values('reddog','2',SYSDATE());
+
+-- itunes
+insert into uuid values('rduvalwa2@comcast.net','3',SYSDATE());
+insert into password values('blu4jazz','3',SYSDATE());
+
+-- snoisle
+insert into uuid values('29067012017865','4',SYSDATE());
+insert into password values('duval','4',SYSDATE());
+
+-- 5 allegis
+insert into uuid values('532549701','5',SYSDATE());
+insert into password values('blu4jazz','5',SYSDATE());
+
+-- verizon_emp
+insert into uuid values('rduvalwa2','6',SYSDATE());
+insert into password values('blu4jazz','6',SYSDATE());
+-- Work_Source
+insert into uuid values('rduvalwa2','7',SYSDATE());
+insert into password values('reddog','7',SYSDATE());
+
+-- 8 mysql
+insert into uuid values('rduvalwa2@comcast.net','8',SYSDATE());
+insert into password values('blu4jazz','8',SYSDATE());
+
+-- 9 oracle
+insert into uuid values('rduvalwa2','9',SYSDATE());
+insert into password values('reddog','9',SYSDATE());
+
+-- 10 community_transit
+insert into uuid values('rduvalwa2@comcast.net','10',SYSDATE());
+insert into password values('blu4jazz','10',SYSDATE());
+
