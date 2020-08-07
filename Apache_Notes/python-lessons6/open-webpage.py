09795bf1b16d5770696e2a3ab01d6ed04495eea2
rduvalwa2 <rduvalwa2@maxbookpro17osx.hsd1.wa.comcast.net>
2020-08-06 22:15:00 -0700

    sync up note from Mac osx 17

---
+++ Apache_Notes/python-lessons6/open-webpage.py
@@ -0,0 +1,10 @@
+# open-webpage.py
+
+import urllib.request, urllib.error, urllib.parse
+
+url = 'http://www.oldbaileyonline.org/browse.jsp?id=t17800628-33&div=t17800628-33'
+
+response = urllib.request.urlopen(url)
+webContent = response.read()
+
+print(webContent[0:300])
---
+++ Apache_Notes/python-lessons6/trial-content.py
@@ -0,0 +1,10 @@
+# trial-content.py
+
+import urllib.request, urllib.error, urllib.parse, obo
+
+url = 'http://www.oldbaileyonline.org/browse.jsp?id=t17800628-33&div=t17800628-33'
+
+response = urllib.request.urlopen(url)
+HTML = response.read()
+
+print(obo.stripTags(HTML))
